89a92283dd5d965ff9b8c234b42d2423fa202b86
Brian Kwon <bkwon94@gmail.com>
2019-06-25 18:56:26 -0400

    Set up navbar and basic styling

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import { NavHashLink as NavLink } from 'react-router-hash-link';
 
 const About = () => {
   return (
     <div id="about">
       <section className="container about-container">
-        <ul
-          className="nav"
+        <div
+          className="nav-container"
           data-aos="fade-down"
           data-aos-delay="300"
           data-aos-duration="300"
         >
-          <li>Home</li>
-          <li>About</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
+            <NavLink smooth to="#main" className="nav-item">Home</NavLink>
+            <NavLink smooth to="#about" className="nav-item">About</NavLink>
+            <NavLink smooth to="#projects" className="nav-item">Work</NavLink>
+            <NavLink smooth to="#contact" className="nav-item">Contact</NavLink>
+        </div>
       </section>
     </div>
   )
