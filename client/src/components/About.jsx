99f7d5ccb0ce4c4cf96e80f1c21a406ee49c0d62
Brian Kwon <bkwon94@gmail.com>
2019-06-28 14:47:50 -0400

    Add intersection observer to keep track of div in viewport

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -3,6 +3,21 @@ import { useState, useEffect } from 'react';
 
 const About = ({ currentSection, setSection }) => {
 
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5
+  }
+  const observer = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {
+      setSection('about');
+    }
+  }, options)
+
+  useEffect(() => {
+    observer.observe(document.querySelector('#about'));
+  }, [])
+
   return (
     <div id="about">
       <section className="container about-container">
--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-const Projects = () => {
+const Projects = ({ currentSection, setSection }) => {
+
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5
+  }
+  const observer = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {
+      setSection('projects');
+    }
+  }, options)
+
+  useEffect(() => {
+    observer.observe(document.querySelector('#projects'));
+  }, [])
 
   return (
     <div id="projects">
-      <section className="container about-container">
+      <section className="container projects-container">
         <div>PROJECTS</div>
       </section>
     </div>
