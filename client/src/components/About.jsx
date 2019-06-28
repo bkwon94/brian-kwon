e2ccbe6297dc10d99b52b9babf3ef764ea74346e
Brian Kwon <bkwon94@gmail.com>
2019-06-28 17:07:38 -0400

    Install lazy load package and add grid display for containers in css

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import LazyLoad from 'react-lazyload';
 
 const About = ({ currentSection, setSection }) => {
 
+  // observer will keep track of what user sees on screen
   const options = {
     root: null,
     rootMargin: '0px',
@@ -13,7 +15,8 @@ const About = ({ currentSection, setSection }) => {
       setSection('about');
     }
   }, options)
-
+  // observe when user reaches 65% of the about div
+  // when this happens, set current section accordingly
   useEffect(() => {
     observer.observe(document.querySelector('#about'));
   }, [])
@@ -21,6 +24,9 @@ const About = ({ currentSection, setSection }) => {
   return (
     <div id="about">
       <section className="container about-container">
+        <LazyLoad height={200} offset={100}>
+          <img id="profile-image" src="./images/profile.jpg" alt="profile photo"/>
+        </LazyLoad>
       </section>
     </div>
   )
